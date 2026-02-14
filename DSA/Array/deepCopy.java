a7fc128c3107dc9dfb6985f512ed2e87101ddf9c
Kisan <kisandas13@gmail.com>
2026-02-14 17:50:20 +0530

    deep copy

--- DSA/Array/deepCopy.java
+++ DSA/Array/deepCopy.java
@@ -11,6 +11,12 @@ public class deepCopy {
         int[] deepArr = Arrays.copyOf(arr, arr.length);
         deepArr[3] = 56;
 
-        System.out.println(arr[3] + " " + shalArr[3] + " " + deepArr[3]);
+        int[] deepArr1 = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++)
+            deepArr1[i] = arr[i];
+        deepArr1[3] = 100;
+
+        System.out.println(arr[3] + " " + shalArr[3] + " " + deepArr[3] + " " + deepArr1[3]);
     }
 }
