10d708d94bb576e513cfd26bd0514e5eac793008
Kisan <kisandas13@gmail.com>
2026-02-14 18:13:05 +0530

    linear search

---
+++ DSA/Array/linearSearch.java
@@ -0,0 +1,30 @@
+package DSA.Array;
+
+import java.util.Scanner;
+
+public class linearSearch {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+
+            int[] arr = { 2, 4, 8, 71, 7, 98 };
+
+            System.out.print("Enyter the target: ");
+            int target = sc.nextInt();
+
+            boolean flag = false;
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == target) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag) {
+                System.out.println("Target exists in array");
+            } else {
+                System.out.println("Target does not exist in array");
+            }
+        }
+    }
+}
