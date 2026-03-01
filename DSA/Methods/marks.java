e3cda65cafcf73cf4fb3f82e3cca97e49062ce14
kisandas13 <kisan.das@datasignsfintech.com>
2026-03-01 15:43:15 +0530

    getter and setter

---
+++ DSA/Methods/marks.java
@@ -0,0 +1,26 @@
+package DSA.Methods;
+
+public class marks {
+
+    public static class StudentData {
+        String name;
+        int rno;
+        int[] marks;
+
+        StudentData(int s) {
+            marks = new int[s];
+        }
+
+        StudentData(int[] sArr) {
+            marks = sArr;
+        }
+    }
+
+    static void main() {
+        StudentData s1 = new StudentData(4);
+        StudentData s2 = new StudentData(3);
+
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        StudentData s3 = new StudentData(arr);
+    }
+}
---
+++ DSA/OOP/constructor.java
@@ -0,0 +1,46 @@
+package DSA.OOP;
+
+public class constructor {
+
+    public static class Car {
+        int seats;
+        String name;
+        double length;
+
+        Car() { //this is default constructor work as new Car()
+        }
+
+        Car(int seats, String name, double l) {
+            //now instead of assiging manually like c1.seats, c1.name we can directly pass as argument
+            this.seats = seats;
+            this.name = name;
+            length = l;
+        }
+
+        Car(String name, double l, int seats) {
+            //now instead of assiging manually like c1.seats, c1.name we can directly pass as argument
+            this.seats = seats;
+            this.name = name;
+            length = l;
+        }
+
+        void print() {
+            System.out.println("name " + name + " Seats " + seats + " length " + length + "m");
+        }
+    }
+
+    static void main() {
+        Car c1 = new Car(6, "Harrier", 5.5);
+        c1.print();
+
+        Car c2 = new Car(4, "Elentra", 6.0);
+        c2.print();
+
+        Car c3 = new Car("Elentra", 6.0, 4);
+        c2.print();
+
+        Car c4 = new Car();
+
+    }
+
+}
