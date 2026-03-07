8dd5cccd6f25a6e20688652a3ba70df770ac12e3
Kisan <kisandas13@gmail.com>
2026-03-07 15:19:03 +0530

    07/03/2026

--- DSA/LinkLIst/LinkedListClass.java
+++ DSA/LinkLIst/LinkedListClass.java
@@ -12,6 +12,32 @@ class Node { //user defined data type
 class LinkedList {
     Node head; // null
     Node tail; // null
+    int size;
+
+    boolean search(int val) {
+        if (head == null) return false;
+
+        Node temp = head;
+        while (temp != null) {
+            if (temp.val == val) return true;
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    int search1(int val) {
+        if (head == null) return -1;
+
+        Node temp = head;
+        int idx = 0;
+        while (temp != null) {
+            if (temp.val == val) return idx;
+            temp = temp.next;
+            idx++;
+        }
+        return -1;
+    }
+
 
     void addAtTail(int val) {
         Node temp = new Node(val);
@@ -20,6 +46,8 @@ class LinkedList {
             tail.next = temp;
             tail = temp;
         }
+
+        size++;
     }
 
     void display() {
@@ -35,37 +63,58 @@ class LinkedList {
 
     void addAtHead(int val) {
         Node temp = new Node(val);
+        if (tail == null) head = tail = temp;
         temp.next = head;
         head = temp;
+        size++;
     }
 
     void addAtMiddle(int idx, int val) {
-        Node temp = new Node(val);
-        Node valatIndex = null;
-        Node valatNextIndex = null;
+        if (idx < 0 || idx >= size) {
+            System.out.println("Index out of range");
+        } else if (idx == 0) addAtHead(val);
+        else if (idx == size) addAtTail(val);
+        else {
+            Node temp = head;
+            Node newNode = new Node(val);
 
-        for (int i = 1; i <= idx; i++) {
-            valatIndex = head.next;
-            valatNextIndex = valatIndex.next;
+            for (int i = 1; i < idx; i++) {
+                temp = temp.next;
+            }
+
+            newNode.next = temp.next;
+            temp.next = newNode;
+            size++;
         }
-
-        valatIndex.next = temp;
-        temp.next = valatNextIndex;
 
     }
 
     void deleteAtHead() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
         head = head.next;
+        size--;
     }
 
     void deleteAtMiddle(int idx) {
-        Node valAtIndex = head;
+        Node temp = head;
 
-        for (int i = 1; i < idx - 1; i++) {
-            valAtIndex = head.next;
+        if (idx < 0 || idx >= size) {
+            System.out.println("Index out of range");
+        } else if (idx == 0) {
+            deleteAtHead();
+        } else {
+            for (int i = 1; i < idx; i++) {
+                temp = temp.next;
+            }
+
+            temp.next = temp.next.next;
+            if (idx == size - 1) tail = temp;
+
+            size--;
         }
-
-        valAtIndex.next = valAtIndex.next.next;
     }
 }
 
@@ -93,5 +142,8 @@ public class LinkedListClass {
 
         ll.deleteAtHead();
         ll.display();
+
+        System.out.println(ll.search(10));
+        System.out.println(ll.search1(90));
     }
 }
