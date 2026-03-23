3acd3c653b36e71e612b367f64895ec4b94c141a
Kisan <kisandas13@gmail.com>
2026-03-23 16:13:29 +0530

    setter injection

--- SpringOne/src/main/java/org/example/Alien.java
+++ SpringOne/src/main/java/org/example/Alien.java
@@ -1,11 +1,19 @@
 package org.example;
 
 public class Alien {
+    private int age;
     Alien(){
         System.out.println("Object created");
     }
 
-    int age;
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        System.out.println("setter called....");
+        this.age = age;
+    }
 
     public void code(){
         System.out.println("coding....");
--- SpringOne/src/main/java/org/example/App.java
+++ SpringOne/src/main/java/org/example/App.java
@@ -8,14 +8,14 @@ public class App
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");//creates container
-        Alien obj = (Alien) context.getBean("alien1", Alien.class);
+        Alien obj = context.getBean("alien1", Alien.class);
         obj.code();
-        obj.age = 20;
-        System.out.println(obj.age);
+//        obj.setAge(20);
+        System.out.println(obj.getAge());
 
-        Alien obj1 = (Alien) context.getBean("alien1", Alien.class); //object is created one time  but obj and obj1 is referring to that
-        obj1.code();
-        System.out.println(obj1.age);
+//        Alien obj1 = (Alien) context.getBean("alien1", Alien.class); //object is created one time  but obj and obj1 is referring to that
+//        obj1.code();
+//        System.out.println(obj1.getAge());
 
     }
 }
