c6215049245900d0b609bfcec1b2fe757b029368
Kisan <kisandas13@gmail.com>
2026-03-28 20:26:46 +0530

    springboot concept

---
+++ SpringBootSample/src/main/java/org/example/springbootsample/Computer.java
@@ -0,0 +1,6 @@
+package org.example.springbootsample;
+
+public interface Computer {
+
+    public void compile();
+}
--- SpringBootSample/src/main/java/org/example/springbootsample/SpringBootSampleApplication.java
+++ SpringBootSample/src/main/java/org/example/springbootsample/SpringBootSampleApplication.java
@@ -1,34 +1,67 @@
 package org.example.springbootsample;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-class Laptop{
-    public void compile(){
-        System.out.println("compile");
+class Laptop implements Computer {
+    public void compile() {
+        System.out.println("Laptop compile");
     }
 }
 
 @Component
-class Alien{
+@Primary
+class Desktop implements Computer {
+    public void compile() {
+        System.out.println("Desktop compile");
+    }
+}
+
+@Component
+class Alien {
+    @Value("24")
+    private int age;
+    private Computer com;
+
+    public Computer getCom() {
+        return com;
+    }
 
     @Autowired
-    Laptop laptop;
-    public void coding(){
+    @Qualifier("laptop")
+    public void setCom(Computer com) {
+        System.out.println("set com");
+        this.com = com;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void coding() {
 //        System.out.println("Alien");
-        laptop.compile();
-    };
+        com.compile();
+    }
+
+    ;
 }
 
 @SpringBootApplication
 public class SpringBootSampleApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context =  SpringApplication.run(SpringBootSampleApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringBootSampleApplication.class, args);
         Alien alien = context.getBean(Alien.class);
         alien.coding();
 
