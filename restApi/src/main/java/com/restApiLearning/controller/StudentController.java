7e9e340dc07be0e598c2873b475dc9a486de9707
Kisan <kisandas13@gmail.com>
2026-04-08 11:45:00 +0530

    connecting DB

--- restApi/src/main/java/com/restApiLearning/controller/StudentController.java
+++ restApi/src/main/java/com/restApiLearning/controller/StudentController.java
@@ -1,14 +1,24 @@
 package com.restApiLearning.controller;
 
 import com.restApiLearning.DTO.StudentDTO;
+import com.restApiLearning.entity.Student;
+import com.restApiLearning.repository.StudentsRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
 
+    private final StudentsRepository studentsRepository;
+
+    private StudentController(StudentsRepository studentsRepository) {
+        this.studentsRepository = studentsRepository;
+    }
+
     @GetMapping("/student")
-    public StudentDTO getStudent() {
-        return new StudentDTO(12L, "Sagar", "abc@gmail.com");
+    public List<Student> getStudent() {
+        return studentsRepository.findAll();
     }
 }
